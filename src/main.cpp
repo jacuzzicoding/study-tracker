4e8aa28fe8b8e3f516a8f9ce214c0bd937238bb6
Brock Jenkinson <brockjenkinson@gmail.com>
2024-10-26 16:24:38 -0400

    Moved welcome message to tracker.cpp as a function

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,7 @@
 #include "tracker.h"
 
 int main() {
-    //putting this here for now, but i'll move it to a different file later once we actually get our functions set up
-    std::cout << "Hello! Welcome to Sebastian and Brock's study tracker!" << std::endl; 
-    return 0;
+    welcomeMessage(); //this will print the welcome message
 
     loadSubjects(); //this will load the subjects from the file, using the function from tracker.cpp
 }
