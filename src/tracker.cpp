ffdb7faccf35e652ce702e9e00fffff6b69c48cd
Brock Jenkinson <brockjenkinson@gmail.com>
2024-10-26 15:55:17 -0400

    Wrote the basic structure for our functions, basically in name only for now. Will flesh out more soon once I wrap my head around the whole project

---
+++ src/tracker.cpp
@@ -0,0 +1,18 @@
+#include "tracker.h"
+#include <iostream>
+#include <fstream>  /* For file operations
+assuming that we will be using save files to store our subjects so you can use the program more than once, lol */
+
+
+//here we will put the actual function definitions for main.cpp to use (protypes in tracker.h)
+void loadSubjects() {
+    std::cout << "Loading subjects..." << std::endl;
+}
+
+void saveSubjects() {
+    std::cout << "Saving subjects..." << std::endl;
+}
+
+void addSubject() {
+    std::cout << "Adding a subject..." << std::endl;
+}
