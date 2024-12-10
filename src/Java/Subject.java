c070837dc3ed4565d977ef589a30703f17401922
Brock Jenkinson <brockjenkinson@gmail.com>
2024-12-10 15:05:59 -0500

    Refactor Subject class to Java

--- src/Java/Subject.java
+++ src/Java/Subject.java
@@ -4,13 +4,18 @@
 
 
 // Example struct for a Subject object just to get us started (C++ CODE MUST BE CHANGED)
-struct Subject {
-    std::string name;
-    std::vector<std::string> tasks;
-    int hoursStudied;
+public class Subject { //public class Subject because it will be used in other classes
+    private String name;
+    private ArrayList<String> tasks;
+    private int hoursStudied;
     // int grade; // Uncomment if needed later
 
-    // Constructor to initialize name and hoursStudied
-    Subject(const std::string& name = "", int hoursStudied = 0)
-        : name(name), hoursStudied(hoursStudied) {}
-};
+    // Constructor to initialize name, task and hoursStudied
+ public Subject(String name, int hoursStudied) {
+    this.name = name;
+    this.tasks = new ArrayList<>();
+    this.hoursStudied = hoursStudied;
+}
+
+// Getters and setters will go here
+}
